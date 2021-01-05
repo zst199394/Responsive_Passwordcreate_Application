abdcc4340b224b98710921a79ebce863a7444789
shuting <zst9394@outlook.com>
2021-01-05 16:52:58 -0500

    var

--- script.js
+++ script.js
@@ -1,6 +1,13 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
+// Add a series of prompts for password criteria
+var confirmLength = prompt("How long is your password?(8~128)");
+var confirmLowercase = confirm("Do you want lowercase?");
+var confirmUppercase = confirm("Do you want uppercase?");
+var confirmNumeric = confirm("Do you want numeric?");
+var confirmSpecial = confirm("Do you want special characters?");
+
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
