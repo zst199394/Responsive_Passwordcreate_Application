9dafc45f7d38666f0ffc7cf888ac6e85ef314da3
shuting <zst9394@outlook.com>
2021-01-05 18:12:02 -0500

    add arrays

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 // Assignment Code
-var lowercase = ["a","b","c","d","e","f","g","h","i",];
-var uppercase =["A","B"];
-var numbers =["0","1","2"];
-var special =["!","@","#","$",]
+var lowercase = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
+var uppercase =["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
+var numbers =["0","1","2","3","4","5","6","7","8","9"];
+var special =["!","@","#","$","%","&","*","+","=","?","<",">"]
 
 
 // Add a series of prompts for password criteria
@@ -28,7 +28,6 @@ function typeCharacter(){
   //var confirmNumeric = confirm("Do you want numeric?");
   //var confirmSpecial = confirm("Do you want special characters?");
 }
-
  else{
   console.log("You already choose.")
 }
