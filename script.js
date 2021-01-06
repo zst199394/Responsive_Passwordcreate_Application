ab19429abee6fedb9d563dc59de7b9e0334e8b37
shuting <zst9394@outlook.com>
2021-01-06 00:20:49 -0500

    isNaN Box123

--- script.js
+++ script.js
@@ -4,61 +4,83 @@ var uppercase =["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P",
 var numbers =["0","1","2","3","4","5","6","7","8","9"];
 var special =["!","@","#","$","%","&","*","+","=","?","<",">"];
 
-
 // Add a series of prompts for password criteria
 // Make sure choose at leaset one type of characters
 function choosePassword(){
   var confirmLength = prompt("How long is your password?(8~128)");
-  if(isNaN(confirmLength) && confirmLength<8 && confirmLength>128){
-    alert("It has to be between 8 to 128 !!!")
+  // create if statement to check confirmLength is a num!!!!!! ??????????
+  if(isNaN(confirmLength)){
+    alert("You need to put a number!!!!!")
+  } 
+  else if(isNaN(confirmLength)==false && confirmLength<8 && confirmLength>128){
+    alert("It has to be between 8 to 128 !!!");
     return;
   }
-  // create if statement to check confirmLength is a num!!!!!!activity
+
+  else{
   var confirmLowercase = confirm("Do you want lowercase?");
   var confirmUppercase = confirm("Do you want uppercase?");
   var confirmNumeric = confirm("Do you want numeric?");
   var confirmSpecial = confirm("Do you want special characters?");
-
- if(confirmLowercase==false && confirmUppercase==false && confirmNumeric==false && confirmSpecial==false){
-  console.log("You haven't choose any one!!!!")
-  alert("You have to choose at least one!!!!!")
+  }
+  if(confirmLowercase==false && confirmUppercase==false && confirmNumeric==false && confirmSpecial==false){
+  console.log("You haven't choose any one!!!!");
+  alert("You have to choose at least one!!!!!");
   return;
-}
+ }
  else{
   console.log("You already choose.")
-}
-var passwordBox =[];
+ }
+ var passwordBox =[];
+
 //add he user choices to the array passwordBox
-if (confirmLowercase){
-     passwordBox.push(lowercase);
-     console.log(passwordBox);
-}
+ if(confirmLowercase) {
+     passwordBox1=passwordBox.concat(lowercase);
+     console.log(passwordBox1);
+     return;
+ }
+ else{
+   passwordBox1=passwordBox;
+   console.log(passwordBox1);
+ }
 if(confirmUppercase){
-   passwordBox.push(uppercase);
-   console.log(passwordBox);
-}
+   passwordBox2=passwordBox1.concat(uppercase);
+   console.log(passwordBox2);
+   return;
+ }
+ else{
+  passwordBox2=passwordBox1;
+  console.log(passwordBox2);
+ }
 if (confirmSpecial){
-  passwordBox.push(special);
+  passwordBox3=passwordBox2.concat(special);
   console.log(passwordBox);
-}
+  return;
+ }
+ else{
+  passwordBox3=passwordBox2;
+  console.log(passwordBox3);
+ }
+
 //create for loop 
 
 for (var i = 0; i < confirmLength; i++) {
-// need to create function generatePassword!!!!!
-var generatePassword = passwordBox[Math.floor(Math.random() * confirmLength)];
+// need to create function generatePassword!!!!!?????????????
+function generatePassword() {
+  var password = passwordBox[Math.floor(Math.random() * confirmLength)];
+  console.log(Password);
 }
 }
 
-// Write password to the #password input
+// Write password to the #password input ??????
 var generateBtn = document.querySelector("#generate");
 function writePassword() {
   choosePassword();
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
-
   passwordText.value = password;
 
 }
 
 // Add event listener to generate button
-generateBtn.addEventListener("click", writePassword )
+generateBtn.addEventListener("click", writePassword)
